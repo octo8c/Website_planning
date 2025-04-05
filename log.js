6b78e842b864d547b0c2f0a12403563dd8f303a9
mercier <christophe.mercier@etu.u-paris.fr>
2025-04-06 01:25:00 +0200

    Version avance de la page login manque la requete sur une bdd

--- log.js
+++ log.js
@@ -2,17 +2,30 @@ $(document).ready(function(){
     $("input#submitLogin").on("click",function(){
         console.log("Tentaive de login(Il manque le moment ou on fait le login)");
     });
-    var tabs = $("Inscription ,Connexion");
-    function switchTabs(event,value){
-        for(val in tabs){
-            $("div#"+val).css('display','none');
-        }
+
+    function existsUsername(username){
+        console.log("FAIREE");
+    }
+    var tabs = $("#Inscription,#Connexion,#mdpOublie");
+    var current = "#Connexion";
+    function switchTabs(value){
+        tabs.each(function(){
+            $(this).css('display','none');
+            console.log(this);
+        });
         $("div#"+value).css('display','block');
 
         console.log("Coucou c'est moi ");
     }
 
-    $("div#tabs button").on("click",switchTabs);
+    
 
+    $("button#inscr").on("click",() =>switchTabs('Inscription'));
+    $("button#conex").on("click",() => switchTabs('Connexion'));
+    $("a#lostPassword").on("click",function (e) { 
+        e.preventDefault();
+        switchTabs('mdpOublie');
+     });
+    console.log("Affichage que c'est bien arrive jusque la ");
     $("div#Connexion").css('display','block');
 })
